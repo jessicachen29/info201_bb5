cd5fa78a2507f30e0b80b45ab631ad678bbb235b
LucySebinLee <sebinlee@uw.edu>
2018-12-01 15:33:38 -0800

    Update

--- ui.R
+++ ui.R
@@ -18,11 +18,10 @@ shinyUI(
                 sidebarLayout(
                   sidebarPanel(
                     radioButtons("plotType", "Plot type",
-                                 c("Scatter"="p", "Line"="l")
+                                 c("Scatter" = "p", "Line" = "l")
                     )
                   ),
                   mainPanel(
-                    plotOutput("plot")
                   )
                 )
        ),
@@ -31,11 +30,10 @@ shinyUI(
                 sidebarLayout(
                   sidebarPanel(
                     radioButtons("plotType", "Plot type",
-                                 c("Scatter"="p", "Line"="l")
+                                 c("Scatter" = "p", "Line" = "l")
                     )
                   ),
                   mainPanel(
-                    plotOutput("plot")
                   )
                 )
        ),
@@ -47,13 +45,92 @@ shinyUI(
                     )
                   ),
                   mainPanel(
-                    plotOutput("plot")
                   )
                 )
        ),
        tabPanel("Data Table",
-                dataTableOutput("table")
-       )
-  ))
-
-
+                fluidRow(
+                  column(3,
+                         selectInput("gender",
+                                     "Gender:",
+                                     c("All",
+                                       unique(as.character(data$gender))))
+                  ),
+                  column(3,
+                         selectInput("NationalITy",
+                                     "Nationality:",
+                                     c("All",
+                                       unique(as.character(data$NationalITy))))
+                  ),
+                  column(3,
+                         selectInput("PlaceofBirth",
+                                     "Place of Birth:",
+                                     c("All",
+                                       unique(as.character(data$PlaceofBirth))))
+                  ),
+                  column(3,
+                         selectInput("StageID",
+                                     "Educational Stages:",
+                                     c("All",
+                                       unique(as.character(data$StageID))))
+                  ),
+                  column(3,
+                         selectInput("GradeID",
+                                     "Grade Levels:",
+                                     c("All",
+                                       unique(as.character(data$GradeID))))
+                  ),
+                  column(3,
+                         selectInput("SectionID",
+                                     "Classroom Student Belongs:",
+                                     c("All",
+                                       unique(as.character(data$SectionID))))
+                  ),
+                  column(3,
+                         selectInput("Topic",
+                                     "Couorse Topic:",
+                                     c("All",
+                                       unique(as.character(data$Topic))))
+                  ),
+                  column(3,
+                         selectInput("Semester",
+                                     "Semester:",
+                                     c("All",
+                                       unique(as.character(data$Semester))))
+                  ),
+                  column(3,
+                         selectInput("Relation",
+                                     "Relation:",
+                                     c("All",
+                                       unique(as.character(data$Relation))))
+                  ),
+                  column(3,
+                         selectInput("ParentAnsweringSurvey",
+                                     "Parent Answering Survey:",
+                                     c("All",
+                                       unique(as.character(data$ParentAnsweringSurvey))))
+                  ),
+                  column(3,
+                         selectInput("ParentschoolSatisfaction",
+                                     "Parent School Satisfaction:",
+                                     c("All",
+                                       unique(as.character(data$ParentschoolSatisfaction))))
+                  ),
+                  column(3,
+                         selectInput("StudentAbsenceDays",
+                                     "Student Absence Days:",
+                                     c("All",
+                                       unique(as.character(data$StudentAbsenceDays))))
+                  ),
+                  column(3,
+                         selectInput("Class",
+                                     "Class:",
+                                     c("All",
+                                       unique(as.character(data$Class))))
+                  )
+                ),
+                mainPanel(
+                  DT::dataTableOutput("table")
+                )
+       ))
+)
