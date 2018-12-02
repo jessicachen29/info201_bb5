0f3074ff0a35dfb1882ec00655292c46b8904419
LucySebinLee <sebinlee@uw.edu>
2018-12-01 16:26:47 -0800

    Update

--- server.R
+++ server.R
@@ -4,11 +4,13 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(plotly)
+library(DT)
 
 data <- data.table::fread("data/xAPI-Edu-Data.csv")
 
-participation <-
-  data %>%
+participation <- data %>%
   select(raisedhands, VisITedResources, AnnouncementsView, Discussion, Class) %>% 
   group_by(Class) %>% 
   summarize(
@@ -25,6 +27,34 @@ shinyServer(function(input, output) {
       geom_col(mapping = aes(x = reorder(Class, c(3, 1, 2)), y = avg_hands)) +
       labs(x = "Grade", y = "Average Hands Raised")
   })
+
+  output$plot2 <- renderPlot({
+    grades <- select(data, Class, NationalITy)
+    
+    high <- filter(grades, Class == 'H')
+    mid <- filter(grades, Class == 'M')
+    low <- filter(grades, Class == 'L')
+    
+    if (input$grades == 'H') {
+      bp <- ggplot(grades, aes(x="", y = nrow(high), fill = NationalITy))+
+        geom_bar(width = 1, stat = "identity")
+      pie <- bp + coord_polar("y", start=0)
+      return(pie)
+      
+    } else if (input$grades == 'M') {
+      bp <- ggplot(grades, aes(x="", y = nrow(mid), fill = NationalITy))+
+        geom_bar(width = 1, stat = "identity")
+      pie <- bp + coord_polar("y", start=0)
+      return(pie)
+      
+    } else {
+      bp <- ggplot(grades, aes(x="", y = nrow(low), fill = NationalITy))+
+        geom_bar(width = 1, stat = "identity")
+      pie <- bp + coord_polar("y", start=0)
+      return(pie)
+      
+    }
+  })
   
   output$plot3 <- renderPlot({
     data <- data %>% 
@@ -32,9 +62,13 @@ shinyServer(function(input, output) {
       group_by(Class) %>% 
       count(ParentAnsweringSurvey) %>% 
       mutate(percentage = n / sum(n) * 100)
+    data$Class <- factor(data$Class, levels = c("L", "M", "H"))
     
     ggplot(data, aes(x = Class, y = percentage, fill = ParentAnsweringSurvey)) + 
-      geom_bar(stat = "identity")
+      geom_bar(stat = "identity") +
+      labs(title = "Parental Involvement by Students' Grades") +
+      xlab ("Grade Level") +
+      ylab("Percentage (%)")
   })
   
   output$table <- DT::renderDataTable(DT::datatable({
